09636ac31634f23893135d8dcfa308c95d105d08
David <david.github@gmail.com>
2011-05-04 01:14:05 -0700

    Allow custom status with status_ttl e.g. you can save http status.

--- lib/canonicurl/cache.rb
+++ lib/canonicurl/cache.rb
@@ -1,28 +1,29 @@
 require 'digest/md5'
 require 'redis'
 
-module Canonicurl  
+module Canonicurl
   class Cache
     CANONICAL  = 'C'
     ERROR      = 'E'
     RESOLVING  = 'R'
 
     TTL = 60 * 60 * 24 * 90 # 90 days ~ 3 months
-    REDIRECTS = 5
-    CONNECTION_TIMEOUT = 5
+    ERROR_TTL = 60 * 60 # 1 hour
     KEY_PREFIX = 'curl:'
 
-    attr_accessor :db, :ttl, :timeout, :redirects
+    attr_accessor :db, :ttl, :error_ttl, :status_ttl
     attr_reader :key_prefix
 
     def self.url(code_or_url)
-      code_or_url && code_or_url.size > 1 ? code_or_url : nil
+      code_or_url && code_or_url.size > 3 ? code_or_url : nil
     end
 
 
     def initialize(options={})
       @db         = options[:db] || Redis.connect
       @ttl        = options[:ttl] || TTL
+      @error_ttl  = options[:error_ttl] || ERROR_TTL
+      @status_ttl = options[:status_ttl] || ERROR_TTL
       @timeout    = options[:timeout] || CONNECTION_TIMEOUT
       @redirects  = options[:redirects] || REDIRECTS
       @key_prefix = options[:key_prefix] || KEY_PREFIX
@@ -37,17 +38,17 @@ module Canonicurl
     def fetch(url, resolver)
       k = key(url)
 
-      # New URL resolve it
+      # New URL, resolve it
       if @db.setnx(k, RESOLVING) #  lock it if key doesn't exist
         return resolve(url, k, resolver)
       end
 
       result = @db.get(k)
-      if !result.nil? && result.size > 1
-        return result # Found old noncanonical URL mapping
+      if !result.nil? && result.size > 3
+        return result # Found cached noncanonical URL mapping
       end
 
-      # CANONICAL, ERROR or RESOLVING
+      # CANONICAL, ERROR, RESOLVING, custom status, or nil
       result == CANONICAL ? url : result
     end
 
@@ -71,24 +72,24 @@ module Canonicurl
     private
 
     def resolve(url, url_key, resolver)
-      @db.set(url_key, RESOLVING)
       begin
-        canonical_url = resolver.call(url).to_s
+        resolved = resolver.call(url).to_s
       rescue Exception => e
-        @db.set(url_key, ERROR)
+        @db.setex(url_key, @error_ttl, ERROR)
         raise e
       end
 
-      if canonical_url.size > 1
-        set url, canonical_url, url_key
-        canonical_url
-      else
-        if canonical_url.size == 1 && ![CANONICAL, ERROR, LOCKED, RESOLVING].include?(canonical_url)
-          @db.set(url_key, canonical_url) # save status
-          canonical_url
-        else
-          @db.set(url_key, ERROR) # error
+      if resolved.size > 3
+        set url, resolved, url_key
+        resolved
+      else # resolver can return status code that is less than 3 characters long
+        # any internal status code is always marked as ERROR w/ error_ttl
+        if [CANONICAL, ERROR, RESOLVING].include?(resolved)
+          @db.setex(url_key, @error_ttl, ERROR)
           ERROR
+        else # status / acknowleged error (e.g. 500) is marked w/ given status code and status_ttl
+          @db.setex(url_key, @status_ttl, resolved)
+          resolved
         end
       end
     end
